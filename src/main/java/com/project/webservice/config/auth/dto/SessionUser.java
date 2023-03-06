08f006f0f23076955286cf3428808485529f42ca
SunKyoung Yoon <yskkjdh@gmail.com>
2023-03-06 20:23:37 +0900

    SessionUser에 implements Serializable 추가 (직렬화 기능을 가지도록)

--- src/main/java/com/project/webservice/config/auth/dto/SessionUser.java
+++ src/main/java/com/project/webservice/config/auth/dto/SessionUser.java
@@ -3,8 +3,10 @@ package com.project.webservice.config.auth.dto;
 import com.project.webservice.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
     private String name;
     private String email;
     private String picture;
